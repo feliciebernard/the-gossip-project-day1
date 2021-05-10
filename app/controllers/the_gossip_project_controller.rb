0738b9305a196d01b92b0c3932322e79cb6be449
nicolas <nicopelletier39@gmail.com>
2021-05-10 17:34:14 +0200

    update

--- app/controllers/the_gossip_project_controller.rb
+++ app/controllers/the_gossip_project_controller.rb
@@ -6,28 +6,11 @@ class TheGossipProjectController < ApplicationController
   end
 
   def welcome
-    puts "$" * 60
-    puts "Voici le message de l'URL :"
-    puts  params[:first_name]
-    puts "$" * 60
     end
 
   def gossip
     @gossip = Gossip.find(params[:gossip_id])
     puts params
-
-
-      #partie freestyle sid
-    @titre = []
-    @contenu = []
-
-    for i in 0..gossips.length
-    titre = gossips[i].title
-    contenu = gossips[i].content
-    end
-      #fin partie freestyle sid
-
-
   end
 
 end
