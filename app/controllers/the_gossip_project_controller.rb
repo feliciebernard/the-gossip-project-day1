55a5ab9ff33a0e7341709a9fe27f20448eb45295
nicolas <nicopelletier39@gmail.com>
2021-05-10 17:15:54 +0200

    Co-authored-by: Loick Jouan <ltevidence@users.noreply.github.com>

--- app/controllers/the_gossip_project_controller.rb
+++ app/controllers/the_gossip_project_controller.rb
@@ -4,4 +4,30 @@ class TheGossipProjectController < ApplicationController
 
   def contact
   end
+
+  def welcome
+    puts "$" * 60
+    puts "Voici le message de l'URL :"
+    puts  params[:first_name]
+    puts "$" * 60
+    end
+
+  def gossip
+    @gossip = Gossip.find(params[:gossip_id])
+    puts params
+
+
+      #partie freestyle sid
+    @titre = []
+    @contenu = []
+
+    for i in 0..gossips.length
+    titre = gossips[i].title
+    contenu = gossips[i].content
+    end
+      #fin partie freestyle sid
+
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'the_gossip_project/team'
-  get 'the_gossip_project/contact'
+  get 'the_gossip_project/team', to: 'the_gossip_project#team'
+  get 'the_gossip_project/contact', to: 'the_gossip_project#contact'
+  get '/the_gossip_project/index', to: 'the_gossip_project#index'
+  get '/the_gossip_project/:gossip_id', to: 'the_gossip_project#gossip'
+  get '/the_gossip_project/:first_name', to: 'the_gossip_project#welcome'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
